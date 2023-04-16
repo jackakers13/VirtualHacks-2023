43c7d5f165d01f14115e5a03228420204ff1529f
Jack Akers <jackakers13@gmail.com>
2023-04-16 17:57:50 -0500

    Update game.js
    Update image file names

--- game.js
+++ game.js
@@ -1,13 +1,13 @@
 // Define array of feather image URLs
 const featherImages = [
-  "image1.jpg",
-  "image2.jpg",
-  "image3.jpg",
-  "image4.jpg",
-  "image5.jpg",
-  "image6.jpg",
-  "image7.jpg",
-  "image8.jpg"
+  "image-1.png",
+  "image-2.jpeg",
+  "image-3.jpeg",
+  "image-4.jpeg",
+  "image-5.jpeg",
+  "image-6.jpeg",
+  "image-7.jpeg",
+  "image-8.jpeg"
 ];
 
 // Shuffle feather image array
